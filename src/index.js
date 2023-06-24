e5606960d1ce4c3946243a48dcb826cb59d6dcbe
root <root@DESKTOP-6I3RBRC>
2023-06-24 15:43:39 +0300

    Added Form Details

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ function fetchMenu(){
     return fetch('http://localhost:3000/ramens')
     .then(response => response.json())
     .then(data => getMenu(data))
-}
-
-// DOM Manipulation to add menu  
-function getMenu(data) {
+ }
+ // DOM Manipulation to add menu  
+ function getMenu(data) {
     data.forEach(ramens => {
    const menuPic = document.createElement('img')
    const ramenMenu = document.querySelector('#ramen-menu')
@@ -22,14 +21,46 @@ function getMenu(data) {
  menuPic.addEventListener('click', display)
  function display(){
  let picDisplay = document.getElementsByClassName('detail-image')
- picDisplay.src = `${ramens.image}`
+ picDisplay[0].src = `${ramens.image}`
  let picName = document.getElementsByClassName('name')
- picName.innerHTML = `${ramens.name}`
+ picName[0].textContent = `${ramens.name}`
  let restaurant = document.getElementsByClassName("restaurant")
- restaurant.innerHTML = `${ramens.restaurant}`
+ restaurant[0].textContent = `${ramens.restaurant}`
+ let rating = document.getElementById('rating-display')
+ rating.textContent = `${ramens.rating}`
+ let comment = document.getElementById('comment-display')
+ comment.textContent = `${ramens.comment}`
+
+ //add event listener to form
+
+ 
+ let form = document.getElementById('new-ramen')
+ form.addEventListener('submit', (e) => {e.preventDefault()
+
+ //create new obj for new entry
+ 
+  const formObj = {}
+  formObj.name = document.querySelector('#new-name').value
+  formObj.restaurant = document.querySelector('#new-restaurant').value
+  formObj.image = document.querySelector('#new-image').value
+  let imagePic = document.createElement('img')
+  imagePic.src = formObj.image
+  ramenMenu.append(imagePic)
+  formObj.rating = document.querySelector('#new-rating').value
+  formObj.comment = document.querySelector('#new-comment').value
+ console.log(formObj)
+
+ //const newformImage= document.createElement('img')
+ //newformImage.src = formObj.image
+ //ramenMenu.append(newformImage)
+
+// add new  entry to menu
+}
+)
 }
 }
 )}
+
 
 
 //get data and render menu to Dom
