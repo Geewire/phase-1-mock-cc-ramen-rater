446f8e2e02689c3ac816ce4069b8463b0230cef6
root <root@DESKTOP-6I3RBRC>
2023-06-25 00:00:15 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const ramenMenu = document.querySelector('#ramen-menu')
+let picDisplay = document.getElementsByClassName('detail-image')
+let picName = document.getElementsByClassName('name')
+let restaurant = document.getElementsByClassName("restaurant")
+let rating = document.getElementById('rating-display')
+let comment = document.getElementById('comment-display')
 //Loading DOMContent
 document.addEventListener('DOMContentLoaded', ()=>{
     fetchMenu()
@@ -13,54 +19,43 @@ function fetchMenu(){
  function getMenu(data) {
     data.forEach(ramens => {
    const menuPic = document.createElement('img')
-   const ramenMenu = document.querySelector('#ramen-menu')
    menuPic.src = `${ramens.image}`
    ramenMenu.append(menuPic)
-
- //adding event listner and displaying details
- menuPic.addEventListener('click', display)
- function display(){
- let picDisplay = document.getElementsByClassName('detail-image')
- picDisplay[0].src = `${ramens.image}`
- let picName = document.getElementsByClassName('name')
- picName[0].textContent = `${ramens.name}`
- let restaurant = document.getElementsByClassName("restaurant")
- restaurant[0].textContent = `${ramens.restaurant}`
- let rating = document.getElementById('rating-display')
- rating.textContent = `${ramens.rating}`
- let comment = document.getElementById('comment-display')
- comment.textContent = `${ramens.comment}`
-
- //add event listener to form
-
- 
- let form = document.getElementById('new-ramen')
- form.addEventListener('submit', (e) => {e.preventDefault()
-
- //create new obj for new entry
- 
-  const formObj = {}
-  formObj.name = document.querySelector('#new-name').value
-  formObj.restaurant = document.querySelector('#new-restaurant').value
-  formObj.image = document.querySelector('#new-image').value
-  let imagePic = document.createElement('img')
-  imagePic.src = formObj.image
-  ramenMenu.append(imagePic)
-  formObj.rating = document.querySelector('#new-rating').value
-  formObj.comment = document.querySelector('#new-comment').value
- console.log(formObj)
-
- //const newformImage= document.createElement('img')
- //newformImage.src = formObj.image
- //ramenMenu.append(newformImage)
-
-// add new  entry to menu
+   
+   //adding event listner and displaying details
+   menuPic.addEventListener('click', display)
+   function display(){
+   picDisplay[0].src = `${ramens.image}`
+   picName[0].textContent = `${ramens.name}`
+   restaurant[0].textContent = `${ramens.restaurant}`
+   rating.textContent = `${ramens.rating}`
+   comment.textContent = `${ramens.comment}`
+  }
+})
 }
-)
-}
-}
-)}
+//add event listener to form
+let form = document.getElementById('new-ramen')
+form.addEventListener('submit', (e) => {e.preventDefault()
 
+   //create new obj for new entry
+   const formObj = {}
+   formObj.name = document.querySelector('#new-name').value
+   picName[0].textContent = formObj.name
+   formObj.restaurant = document.querySelector('#new-restaurant').value
+   restaurant[0].textContent = formObj.restaurant
+   formObj.image = document.querySelector('#new-image').value
+   let imagePic = document.createElement('img')
+   imagePic.src = formObj.image
+   ramenMenu.append(imagePic)
+   picDisplay[0].src = imagePic.src
+   formObj.rating = document.querySelector('#new-rating').value
+   rating.textContent = formObj.rating
+   formObj.comment = document.querySelector('#new-comment').value
+   comment.textContent = formObj.comment
+   
+ })
 
-
-//get data and render menu to Dom
+// Extra work -see first image as soon as page loads without clicking
+document.querySelector('.detail-image').src = "./assets/ramen/shoyu.jpg"
+document.querySelector(`.name`).textContent = "Shoyu Ramen"
+document.querySelector(`.restaurant`).textContent = "Nonono"
